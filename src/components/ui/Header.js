8602ce458acf6bef3ca465b0af19cb84a973dd13
EleoRomario <eleo.romario@gmail.com>
2022-05-05 11:39:02 -0500

    header

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -2,8 +2,8 @@ import { useState } from "react";
 
 export const Header = () => {
 	const [bg, setBg] = useState({
-		text: "light",
-		icon: "sunny",
+		text: "dark",
+		icon: "moon",
 	});
 
 	const changeBg = () => {
@@ -11,39 +11,40 @@ export const Header = () => {
 		document.body.classList.contains("darkTheme")
 			? setBg({ text: "light", icon: "sunny" })
 			: setBg({ text: "dark", icon: "moon" });
-	}
+	};
 	const { text, icon } = bg;
 	return (
 		<div className="header-container">
-			<div className="header__navegation">
+			<div className="header__btn-menu">
 				<div className="icon">
-					<ion-icon name="chevron-back-outline"></ion-icon>
-				</div>
-				<div className="icon">
-					<ion-icon name="chevron-forward-outline"></ion-icon>
+					<ion-icon name="grid-outline"></ion-icon>
 				</div>
 			</div>
-			<div className="header__search">
-				<div className="icon">
-					<ion-icon name="search-outline"></ion-icon>
+			<div className="search__container">
+				<div className="search__input">
+					<div className="icon">
+						<ion-icon name="search-outline"></ion-icon>
+					</div>
+					<input
+						type="text"
+						className="search__input"
+						placeholder="Search any song here"
+					/>
 				</div>
-				<input
-					type="text"
-					className="search__input"
-					placeholder="Search any song here"
-				/>
 			</div>
-			<div className="header__notification">
-				<div className="icon">
-					<ion-icon name="notifications-outline"></ion-icon>
+			<div className="header__btns">
+				<div className="header__notification">
+					<div className="icon">
+						<ion-icon name="notifications-outline"></ion-icon>
+					</div>
+					<span className="point__notification"></span>
 				</div>
-				<span className="point__notification"></span>
-			</div>
-			<div className="header__bgbutton" onClick={changeBg}>
-				<span className="icon">
-					<ion-icon name={`${icon}-outline`}></ion-icon>
-				</span>{" "}
-				{text}
+				<div className="header__bgbutton" onClick={changeBg}>
+					<span className="icon">
+						<ion-icon name={`${icon}-outline`}></ion-icon>
+					</span>{" "}
+					{text}
+				</div>
 			</div>
 		</div>
 	);
